cb7d7d942722b74764909226ba3427f92e0da214
Rajveer kaur <krajveer1312gmail.com>
2019-11-08 08:54:30 -0500

    deleting cell has been done

--- rajveer_c0758564_note_p1/NewFolderTableViewController.swift
+++ rajveer_c0758564_note_p1/NewFolderTableViewController.swift
@@ -30,8 +30,7 @@ class NewFolderTableViewController: UITableViewController {
                }
                
                       let okAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-//               let AddItemAction = UIAlertAction(title:"Add Item" , style: .default)
-        let AddItemAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
+    let AddItemAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
             let txtField = alertcontroller.textFields![0]
             self.array_folder!.append(txtField.text!)
             print(self.array_folder!)
@@ -95,15 +94,18 @@ class NewFolderTableViewController: UITableViewController {
 //
 //
 //
-//    // Override to support editing the table view.
-//func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            // Delete the row from the data source
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            array_folder?.remove(at: indexPath.row)
+            tableViewData.reloadData()
+            
+            // Delete the row from the data source
 //            tableView.deleteRows(at: [indexPath], with: .fade)
 //        } else if editingStyle == .insert {
-//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//        }
-//    }
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
 //
 //
 //
@@ -115,13 +117,19 @@ class NewFolderTableViewController: UITableViewController {
 //
 //
 //    // Override to support conditional rearranging of the table view.
-//func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-//        // Return false if you do not want the item to be re-orderable.
-//        return true
-//    }
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+       return true
+   }
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = array_folder?[sourceIndexPath.row]
+//        guard let item = array_folder?[sourceIndexPath.row] else { return array_folder }
+        array_folder?.remove(at: sourceIndexPath.row)
+        array_folder?.insert(item!, at: destinationIndexPath.row)
+    }
 //
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -129,7 +137,7 @@ class NewFolderTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
 
